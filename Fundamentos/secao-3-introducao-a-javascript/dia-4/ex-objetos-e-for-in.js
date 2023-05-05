5e7fbcdb13d57b2f1957c75d4fec1fbfc2b4a9cd
Cayo Yuzo Nakama <cayo.yuzo@gmail.com>
2023-05-05 20:14:59 -0300

    fim ex-1

--- Fundamentos/secao-3-introducao-a-javascript/dia-4/ex-objetos-e-for-in.js
+++ Fundamentos/secao-3-introducao-a-javascript/dia-4/ex-objetos-e-for-in.js
@@ -2,4 +2,28 @@ let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  };
+};
+
+console.log("Bem-vinda, " + info.personagem);
+
+info['recorrente'] = 'Sim';
+console.log("-----");
+for (let key in info){
+  console.log(key);
+}
+
+console.log("-----");
+for (let key in info){
+  console.log(info[key]);
+}
+
+console.log("-----");
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim'
+};
+for (key in info){
+  console.log(info[key] + ' e ' + info2[key]);
+}
