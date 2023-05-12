4121ad39042d289aa6e48f14904d7d9331f97274
Cayo Yuzo Nakama <cayo.yuzo@gmail.com>
2023-05-12 18:17:46 -0300

    ex2

--- Fundamentos/secao-3-javascript-DOM-eventos-e-web-storage/dia-2/script.js
+++ Fundamentos/secao-3-javascript-DOM-eventos-e-web-storage/dia-2/script.js
@@ -1,8 +1,10 @@
 const body = document.querySelector('body');
-
+const main = document.querySelector('main');
 
 const h1 = document.createElement('h1');
 h1.innerText = 'TrybeTrip - Agência de Viagens';
 body.appendChild(h1)
 
+body.appendChild(document.createElement('main'));
+main.classList.add('main-content');
 
